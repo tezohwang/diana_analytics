8799f15cac200d8f778fd822e04d0296bdcfc296
tezohwang <tony.hwang@wizpace.com>
2018-05-21 13:49:53 +0900

    change diana DB address

--- report/config.py
+++ report/config.py
@@ -1,10 +1,10 @@
 DATABASE = {
     "diana": {
-        "uri": "mongodb://wizpace:wizpace0@13.125.239.144:27017/diana",
+        "uri": "mongodb://wizpace:wizpace0@mongo.diana.business:27017/diana",
         "name": "diana"
     },
     "autobidding": {
-        "uri": "mongodb://autobidding:autobid0@13.125.87.196:27017/autobidding",
+        "uri": "mongodb://autobidding:autobid0@bid.diana.business:27017/autobidding",
         "name": "autobidding"
     }
 }
